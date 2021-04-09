6cec1003dacfe7c094b81eaddbe388e4a3935f5d
Anthony Gambale <ants.gambale@gmail.com>
2021-04-09 13:44:22 +1000

    added a matrix object to lin alg module

--- src/camera.py
+++ src/camera.py
@@ -1,17 +1,40 @@
 
 
-# note: see math scratch on github page for context on variable names
+# note: see math sketch on github page for context on variable names
 
 from vector import *
+from math import cos, sin
+
 
 class Camera():
 
     def __init__(self):
 
         self.c = Vec3([0, 0, 0]) # initialize the c vector to be 0,0,0
-
         self.n = Vec3([0, 0, 1]) # normal vector, pointing into the x axis from the origin
-
         self.k = 1 # scalar distance from c to s
+        self.s = self.c.add(self.k * self.n) # define centre of screen, offset from the camera point by a scaled version of normal vector
+
+
+    def translate(self, m):
+        """translate the camera with some velocity, Vec3 m"""
+        self.c = self.c.add(m)
+        self.s = self.s.add(m)
+    
+
+    def turn_x(self, a):
+        """take some angle a in radians and rotate the normal vector. see https://en.wikipedia.org/wiki/Rotation_matrix#In_three_dimensions"""
+        
+        self.s = self.c.add(self.k * self.n) # define centre of screen, offset from the camera point by a scaled version of normal vector
+
+
+    def turn_y(self, a):
+        """take some angle a in radians and rotate the normal vector. see Camera.turn_x for source."""
+    
+        self.s = self.c.add(self.k * self.n) # define centre of screen, offset from the camera point by a scaled version of normal vector
+
+
+    def turn_z(self, a):
+        """take some angle a in radians and rotate the normal vector. see Camera.turn_x for source."""
 
         self.s = self.c.add(self.k * self.n) # define centre of screen, offset from the camera point by a scaled version of normal vector
--- src/vector.py
+++ src/vector.py
@@ -1,5 +1,6 @@
 
-# Vector arithmetic module
+# Linear algebra arithmetic module
+
 
 class Vec():
 
@@ -8,9 +9,11 @@ class Vec():
         # list of elements
         self.elems = e
 
+
     def __str__(self):
         """behaviour for Vec when print() is called on it"""
         return str(self.elems) # print elements
+
 
     def add(self, other):
         """add Vec self to Vec other, elementwise"""
@@ -18,7 +21,8 @@ class Vec():
             return Vec([self.elems[i] + other.elems[i] for i in range(len(self.elems))]) # return a new vector without changing self
         else:
             return "error: in vector addition, vectors did not have the same number of elements"
-    
+
+
     def sub(self, other):
         """add Vec self to Vec other, elementwise"""
         if len(self.elems) == len(other.elems):
@@ -26,9 +30,11 @@ class Vec():
         else:
             return "error: in vector addition, vectors did not have the same number of elements"
 
+
     def scale(self, k):
         """scale Vec self by Num k"""
         return Vec([e * k for e in self.elems]) # return a new vector, and leave self unchanged
+
 
     def dot(self, other):
         """return the dot product of Vec self and Vec other"""
@@ -36,6 +42,26 @@ class Vec():
             return sum([self.elems[i] * other.elems[i] for i in range(len(self.elems))])
         else:
             return "error: in vector dot product, vectors did not have the same number of elements"
+
+
+    def transform(self, matrix):
+        """transform self by a matrix. matrix must have enough columns to fit Vec self, and it must be square so that it maps Vec self back into the same space."""
+        if not (len(matrix) == len(matrix[0]) and len(matrix) == len(self.elems)):
+            return "error: matrix in transformation must be square, and it must have enough columns to fit the vector"
+        
+        output_elems = []
+
+        for row in matrix.rows:
+            output_elems.append(self.dot(row))
+        
+        return Vec(output_elems) # return output without changing the current vector
+
+
+class Matrix():
+
+    def __init__(self, vec_list):
+
+        self.rows = vec_list # list of vectors, where each vector represents a row. this way rows can be iterated over
 
 
 class Vec2(Vec):
