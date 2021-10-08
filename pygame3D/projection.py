49ee1c1d677f053a58477dca2fa3b4ef76ba34a8
Anthony Gambale <ants.gambale@gmail.com>
2021-10-08 12:20:08 +1100

    abstracted away some projection functionality to improve quality

--- pygame3D/projection.py
+++ pygame3D/projection.py
@@ -22,34 +22,40 @@ def project_shape(shp, cam):
 
 def project_line(lin, cam):
     """ take in Line3D lin and Camera cam, and project it to 2D. """
-    Tp1 = project_point(lin.p1, cam) # transform each point
-    Tp2 = project_point(lin.p2, cam)
     # return a dead line if either point is behind the camera
     # ideally these would be filtered out instead of drawn. fix later
     if cam.isBehind(lin.p1) or cam.isBehind(lin.p2):
         return None
+    #if cam.isBehind(lin.p1): lin.p1 = find_intersection(lin.p2, lin.p1, cam)
+    #if cam.isBehind(lin.p2): lin.p2 = find_intersection(lin.p2, lin.p1, cam)
+    # transform each point
+    Tp1 = project_point(lin.p1, cam)
+    Tp2 = project_point(lin.p2, cam)
     return Line2D(Tp1, Tp2) # 2D line of Vec2D points
+
+
+def find_intersection(p1, p2, cam):
+    """draw a line that connects points p1 and p2. return the point on that line that intersects the virtual screen, if there is one."""
+    s = cam.s
+    n = cam.n
+    m = Line3D(p1, p2).gradient() # gradient of the line
+    div = n.dot(m)
+    if div == 0: div = 0.001
+    t = (n.dot(s.sub(p1))) / div
+    x = p1.add(m.scale(t)) # since x = p + t*m
+    return x
 
 
 def project_point(p, cam):
     """ project 3D point onto 3D camera plane, then transform intersection point from 3D camera plane into 2D pixel coordinate space. """
     
-    s = cam.s
-    n = cam.n
-    c = cam.c
+    s = cam.s # center point of screen
+    c = cam.c # location of user's face (offset from screen center)
     bx = cam.bx # basis vectors of screen plane
     by = cam.by
     
-    m = Line3D(p, c).gradient()
-
-    # t is the parameter in the parametric equation of the line.
-    # this specific t value represents the point in space where the line and screen plane cross
-    div = n.dot(m)
-    if div == 0: div = 0.001
-    t = (n.dot(s.sub(p))) / div
-
-    # extract the point x from t
-    x = p.add(m.scale(t)) # since x = p + t*m
+    # find the intersection
+    x = find_intersection(p, c, cam)
 
     # find the relative distance from the centre to x
     d = s.sub(x)
